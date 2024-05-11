5cc5940fca337dd79f93bc9beee088a24d081c1a
Salvatore Rago <salvatorerago373@gmail.com>
2024-05-11 12:51:36 +0200

    VGG pretrained weights inclusion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import torch
+import torchvision
+from torchvision.models import VGG16_Weights
 
 from model.vgg import VGG16
 
@@ -6,9 +8,11 @@ from model.vgg import VGG16
 def vgg_test(device):
     inp = torch.randn((3, 224, 224)).to(device)
 
-    model = VGG16(3, device)
+    model = VGG16(3, include_classifier_part=False, device=device)
+    model.load_imagenet_weights_feature_extr()
+
     r = model(inp)
-    print(r)
+    print(r.size())
 
 
 def main():
@@ -16,7 +20,6 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     vgg_test(device)
-
 
 if __name__ == '__main__':
     main()
--- model/vgg.py
+++ model/vgg.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 import torch
+import torchvision
+from torchvision.models import VGG16_Weights
 
 
 class VGG16(nn.Module):
@@ -8,7 +10,9 @@ class VGG16(nn.Module):
     "VERY DEEP CONVOLUTIONAL NETWORKS FOR LARGE-SCALE IMAGE RECOGNITION".
     """
 
-    def __init__(self, num_classes: int, device: torch.device = None):
+    def __init__(self, num_classes: int,
+                 include_classifier_part: bool = True,
+                 device: torch.device = None):
         """
         Create a VGG16 network for classification, with num_classes possible labels.
         The model input size is 3x224x224.
@@ -60,17 +64,20 @@ class VGG16(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
-        self.layer6 = nn.Sequential(
-            nn.Flatten(start_dim=0),
-            nn.Linear(512 * 7 * 7, 4096, device=device),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(4096, 4096, device=device),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(4096, num_classes, device=device),
-            nn.Softmax(dim=0),
-        )
+        if include_classifier_part:
+            self.layer6 = nn.Sequential(
+                nn.Flatten(start_dim=0),
+                nn.Linear(512 * 7 * 7, 4096, device=device),
+                nn.ReLU(),
+                nn.Dropout(0.5),
+                nn.Linear(4096, 4096, device=device),
+                nn.ReLU(),
+                nn.Dropout(0.5),
+                nn.Linear(4096, num_classes, device=device),
+                nn.Softmax(dim=0),
+            )
+        else:
+            self.layer6 = nn.Identity()
 
     def forward(self, x):
         # out1 = self.layer1(x)
@@ -86,3 +93,16 @@ class VGG16(nn.Module):
         out = self.layer4(out)
         out = self.layer5(out)
         return self.layer6(out)
+
+    def load_imagenet_weights_feature_extr(self):
+        """
+        Loads in the feature extractor part of the current model the weights of the VGG16 model pretrained on ImageNet.
+        """
+        pretrained_model = torchvision.models.vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
+        pretrained_feat_extr = list(filter(lambda layer: isinstance(layer, nn.Conv2d), pretrained_model.features))
+
+        this_feat_extr = self.layer1 + self.layer2 + self.layer3 + self.layer4 + self.layer5
+        this_feat_extr = list(filter(lambda layer: isinstance(layer, nn.Conv2d), this_feat_extr))
+
+        for ind, layer in enumerate(pretrained_feat_extr):
+            this_feat_extr[ind].weight = layer.weight
