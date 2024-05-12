028a3c11c729141509c06ae23cefa9316319c7e0
mattiamaffo <126592486+mattiamaffo@users.noreply.github.com>
2024-05-12 14:35:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import torch
 import torchvision
 from torchvision.models import VGG16_Weights
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
 
+from transformation import Transformation
 from model.vgg import VGG16
 
+def polar(device):
+    #load the image
+    img = plt.imread('sat_img.png')
+    
+    aerial_size = 1200
+    height = 128
+    width = 512
+    # img = np.random.rand(100, 100)
 
+    t = Transformation('polar', aerial_size, height, width)
+    img_polar = t.polar(img)
+    print(f"Shape of the original image: {img.shape} and shape of the polar image: {img_polar.shape}")
+    
+    plt.imshow(img_polar)
+    plt.show()
+    return img_polar
+    
 def vgg_test(device):
     inp = torch.randn((3, 224, 224)).to(device)
 
@@ -18,7 +38,8 @@ def vgg_test(device):
 def main():
     # Device configuration
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+    
+    polar(device)
     vgg_test(device)
 
 if __name__ == '__main__':
