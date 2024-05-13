61b6533859091d4cb8d77094adeb8f2eba8c9208
Salvatore Rago <salvatorerago373@gmail.com>
2024-05-13 20:13:03 +0200

    Dataloaders implementation

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import pandas as pd
 import torch
 import torchvision
+from torch.utils.data import DataLoader
 from torchvision.models import VGG16_Weights
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 
+from model.data import CrossViewDataset
 from transformation import Transformation
 from model.vgg import VGG16
+
 
 def correlation(device):
     H = 3
@@ -36,10 +40,11 @@ def correlation(device):
     print("Similarity Scores: ", similiraty)
     print("Orientation: ", orientation)
 
+
 def polar(device):
-    #load the image
+    # load the image
     img = plt.imread('sat_img.png')
-    
+
     aerial_size = 1200
     height = 128
     width = 512
@@ -48,11 +53,12 @@ def polar(device):
     t = Transformation('polar', aerial_size, height, width)
     img_polar = t.polar(img)
     print(f"Shape of the original image: {img.shape} and shape of the polar image: {img_polar.shape}")
-    
+
     plt.imshow(img_polar)
     plt.show()
     return img_polar
-    
+
+
 def vgg_test(device):
     inp = torch.randn((3, 224, 224)).to(device)
 
@@ -63,13 +69,56 @@ def vgg_test(device):
     print(r.size())
 
 
+def load_data(device):
+    dataset_path = "/Volumes/SALVATORE R/Università/CV/hw_data/cvusa/CVUSA_subset/CVUSA_subset/"
+    trainCSV = "/Volumes/SALVATORE R/Università/CV/hw_data/cvusa/CVUSA_subset/CVUSA_subset/train-19zl.csv"
+    valCSV = "/Volumes/SALVATORE R/Università/CV/hw_data/cvusa/CVUSA_subset/CVUSA_subset/val-19zl.csv"
+
+    # id_list = []
+    # id_idx_list = []
+    # with open(trainCSV, 'r') as file:
+    #     idx = 0
+    #     for line in file:
+    #         data = line.split(',')
+    #         pano_id = (data[0].split('/')[-1]).split('.')[0]
+    #         # satellite filename, streetview filename, pano_id
+    #         id_list.append([data[0].replace('bing', 'polar').replace('jpg', 'png'), data[0], data[1], pano_id])
+    #         # polarmap, bingmap, streetview, id (inputXXX)
+    #         id_idx_list.append(idx)
+    #         idx += 1
+
+    # methods return  batch_sat_polar, batch_sat, batch_grd, batch_segmap, batch_orien
+
+    train_dataset = CrossViewDataset(trainCSV, base_path=dataset_path, device=device)
+    validation_dataset = CrossViewDataset(valCSV, base_path=dataset_path, device=device)
+
+    batch_size = 128
+    training_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
+    # test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    #  Display some samples
+    num_samples = 2
+    fig, axs = plt.subplots(num_samples, 3)
+    for i in range(num_samples):
+        img1, img2, img3 = train_dataset.__getitem__(i)
+        img1, img2, img3 = np.transpose(img1, (1, 2, 0)), np.transpose(img2, (1, 2, 0)), np.transpose(img3, (1, 2, 0)),
+        axs[i][0].imshow(img1)
+        axs[i][1].imshow(img2)
+        axs[i][2].imshow(img3)
+
+    plt.show()
+
+
 def main():
     # Device configuration
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
-    polar(device)
-    correlation(device)
-    vgg_test(device)
+    load_data(device)
+    # polar(device)
+    # correlation(device)
+    # vgg_test(device)
+
 
 if __name__ == '__main__':
     main()
