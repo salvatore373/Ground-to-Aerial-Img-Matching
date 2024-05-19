a3a241a1e367d26edd4152c84ebed907dca7562c
mattiamaffo <126592486+mattiamaffo@users.noreply.github.com>
2024-05-19 18:08:38 +0200

    Update main.py
    segmentation

--- main.py
+++ main.py
@@ -13,65 +13,13 @@ from transformation import Transformation
 from model.vgg import VGG16
 
 import segmentation
-import segmentation2
-
-
-def correlation(device):
-    H = 3
-    Ws = 5
-    Wv = 3
-    C = 2
-
-    Fs1 = np.random.rand(H, Ws, C)
-    Fg1 = np.random.rand(H, Wv, C)
-
-    Fs2 = np.random.rand(H, Ws, C)
-    Fg2 = np.random.rand(H, Wv, C)
-
-    Fs3 = np.random.rand(H, Ws, C)
-    Fg3 = np.random.rand(H, Wv, C)
-
-    Fs4 = np.random.rand(H, Ws, C)
-    Fg4 = np.random.rand(H, Wv, C)
-
-    pairs = [(Fs1, Fg1), (Fs2, Fg2), (Fs3, Fg3), (Fs4, Fg4)]
-
-    t = Transformation('correlation')
-    img_correlation = t.correlation(Fs1, Fg1)
-    print("Correlation Scores: ", img_correlation)
-
-    orientation, similiraty = t.generate_similarity_matrix(pairs)
-    print("Similarity Scores: ", similiraty)
-    print("Orientation: ", orientation)
-
 
 def image_segmentation(device):
     # load the non-segmented image
-    img = plt.imread('data/CVUSA_subset/bingmap/input0000008.png')
+    img_path = "data/CVUSA_subset/bingmap/input0000029.png"
 
-    # load the segmented image
-    seg_img = plt.imread('data\CVUSA_subset\segmap\output0000008.png')
+    segmentation.segmentation(img_path)
 
-    seg_img1 = segmentation.segmentation(device, img)
-    seg_img2 = segmentation2.segmentation(device, img)
-
-    # show the three images
-    fig, axes = plt.subplots(1, 3, figsize=(18, 6))
-
-    axes[0].imshow(seg_img)
-    axes[0].set_title('The original segmented image')
-    axes[0].axis('off')
-
-    axes[1].imshow(seg_img1)
-    axes[1].set_title('First segmentation model')
-    axes[1].axis('off')
-
-    axes[2].imshow(seg_img2)
-    axes[2].set_title('Second segmentation model')
-    axes[2].axis('off')
-
-    plt.tight_layout()
-    plt.show()
 
 def polar(device):
     # load the image
@@ -153,22 +101,19 @@ def polar_tensor(device):
     return img_polar
 
 def vgg_test(device):
-    # inp = torch.randn((3, 224, 224)).to(device)
-    inp = torch.randn((1, 3, 128, 512)).to(device)
+    inp = torch.randn((3, 224, 224)).to(device)
 
-    model = VGG16(3, include_classifier_part=False, circular=True, device=device)
+    model = VGG16(3, include_classifier_part=False, device=device)
     model.load_imagenet_weights_feature_extr()
 
     r = model(inp)
-    r2 = model(inp)
-    q = torch.cat((r, r2), dim=0 if len(r.size()) == 3 else 1)
     print(r.size())
 
 
 def load_data(device):
-    dataset_path = "/Volumes/SALVATORE R/Università/CV/hw_data/cvusa/CVUSA_subset/CVUSA_subset/"
-    trainCSV = "/Volumes/SALVATORE R/Università/CV/hw_data/cvusa/CVUSA_subset/CVUSA_subset/train-19zl.csv"
-    valCSV = "/Volumes/SALVATORE R/Università/CV/hw_data/cvusa/CVUSA_subset/CVUSA_subset/val-19zl.csv"
+    dataset_path = "D:/Università/CV/Ground-to-Aerial-Img-Matching/data/CVUSA_subset"
+    trainCSV = "data/CVUSA_subset/train-19zl.csv"
+    valCSV = "data/CVUSA_subset/val-19zl.csv"
 
     # id_list = []
     # id_idx_list = []
@@ -205,11 +150,10 @@ def load_data(device):
 
     plt.show()
 
-
 def train(device):
-    dataset_path = "/Volumes/SALVATORE R/Università/CV/hw_data/cvusa/CVUSA_subset/CVUSA_subset/"
-    trainCSV = "/Volumes/SALVATORE R/Università/CV/hw_data/cvusa/CVUSA_subset/CVUSA_subset/train-19zl.csv"
-    valCSV = "/Volumes/SALVATORE R/Università/CV/hw_data/cvusa/CVUSA_subset/CVUSA_subset/val-19zl.csv"
+    dataset_path = "data"
+    trainCSV = "data/CVUSA_subset/train-19zl.csv"
+    valCSV = "data/CVUSA_subset/val-19zl.csv"
 
     batch_size = 64
     epochs = 2
@@ -221,18 +165,17 @@ def train(device):
 
     # Trainer()
 
-
 def main():
     # Device configuration
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # load_data(device)
-    # polar(device)
-    img_polar2 = polar_tensor(device)
-    print("Shape of the polarized image: ", img_polar2.shape)
+    #load_data(device)
+    #img_polar = polar(device)
+    #img_polar2 = polar_tensor(device)
+    #print("Shape of the polarized image: ", img_polar2.shape)
     # correlation(device)
-    vgg_test(device)
-    #image_segmentation(device)
+    # vgg_test(device)
+    image_segmentation(device)
 
 
 if __name__ == '__main__':
