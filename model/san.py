e43a328a9230bab03977211d986a79191454623f
Salvatore Rago <salvatorerago373@gmail.com>
2024-05-23 17:16:02 +0200

    SAN remove test code

--- model/san.py
+++ model/san.py
@@ -50,97 +50,6 @@ class SAN(nn.Module):
 
         return (loss_gr_to_sat + loss_sat_to_gr) / 2.0
 
-    def tf_correl(self, vgg_gr, sat_concat):
-        import tensorflow as tf
-        def corr(sat_matrix, grd_matrix):
-            s_h, s_w, s_c = sat_matrix.get_shape().as_list()[1:]
-            g_h, g_w, g_c = grd_matrix.get_shape().as_list()[1:]
-            assert s_h == g_h, s_c == g_c  # devono avere la stessa altezza e lo stesso numero di canali
-
-            def warp_pad_columns(x, n):
-                out = tf.concat([x, x[:, :, :n, :]], axis=2)
-                return out
-
-            n = g_w - 1
-
-            x = warp_pad_columns(sat_matrix, n)
-
-            # Correlation between Fs and Fg (use convolution but transposing the filter = correlation)
-            f = tf.transpose(a=grd_matrix, perm=[1, 2, 3, 0])
-            out = tf.nn.conv2d(input=x, filters=f, strides=[1, 1, 1, 1], padding='VALID')
-            h, w = out.get_shape().as_list()[1:-1]
-
-            assert h == 1, w == s_w
-
-            out = tf.squeeze(out, axis=1)  # shape = [batch_sat, w, batch_grd]
-            # Get the area with the maximum correlation
-            orien = tf.argmax(input=out, axis=1)  # shape = [batch_sat, batch_grd]
-
-            return out, tf.cast(orien, tf.int32)
-
-        def tf_shape(x, rank):
-            static_shape = x.get_shape().with_rank(rank).as_list()
-            dynamic_shape = tf.unstack(tf.shape(input=x), rank)
-            return [s if s is not None else d for s, d in zip(static_shape, dynamic_shape)]
-
-        def crop_sat(sat_matrix, orien, grd_width):
-            batch_sat, batch_grd = tf_shape(orien, 2)  # ritorna le dimensioni dei due batch
-            h, w, channel = sat_matrix.get_shape().as_list()[1:]
-            sat_matrix = tf.expand_dims(sat_matrix, 1)  # shape=[batch_sat, 1, h, w, channel]
-            sat_matrix = tf.tile(sat_matrix, [1, batch_grd, 1, 1, 1])
-            sat_matrix = tf.transpose(a=sat_matrix,
-                                      perm=[0, 1, 3, 2, 4])  # shape = [batch_sat, batch_grd, w, h, channel]
-            orien = tf.expand_dims(orien, -1)  # shape = [batch_sat, batch_grd, 1]
-
-            i = tf.range(batch_sat)
-            j = tf.range(batch_grd)
-            k = tf.range(w)
-
-            x, y, z = tf.meshgrid(i, j, k, indexing='ij')
-            z_index = tf.math.floormod(z + orien, w)
-            x1 = tf.reshape(x, [-1])
-            y1 = tf.reshape(y, [-1])
-            z1 = tf.reshape(z_index, [-1])
-            index = tf.stack([x1, y1, z1], axis=1)
-            sat = tf.reshape(tf.gather_nd(sat_matrix, index), [batch_sat, batch_grd, w, h, channel])
-            index1 = tf.range(grd_width)
-            sat_crop_matrix = tf.transpose(a=tf.gather(tf.transpose(a=sat, perm=[2, 0, 1, 3, 4]), index1),
-                                           perm=[1, 2, 3, 0, 4])
-            # shape = [batch_sat, batch_grd, h, grd_width, channel]
-            assert sat_crop_matrix.get_shape().as_list()[3] == grd_width
-
-            return sat_crop_matrix
-
-        # sat_vgg = tf.random.uniform(shape=[10, 4, 64, 16], minval=0, maxval=1)
-        # grd_vgg = tf.random.uniform(shape=[10, 4, 64, 16], minval=0, maxval=1)
-        sat_vgg = sat_concat
-        grd_vgg = vgg_gr
-
-        sat_vgg_torch = torch.from_numpy(sat_vgg.numpy())
-        grd_vgg_torch = torch.from_numpy(vgg_gr.numpy())
-
-        #permute shape of sat_vgg_torch and grd_vgg_torch: N.B.: Here was the mistake! 
-        sat_vgg_torch = sat_vgg_torch.permute(0, 3, 1, 2)
-        grd_vgg_torch = grd_vgg_torch.permute(0, 3, 1, 2)
-        
-        correlation_out, correlation_orient = self.img_processor.correlation(grd_vgg_torch, sat_vgg_torch) #Before the "correlation_out" shape was (Bs, H, Bg). Now, correctly is (Bs, W, Bg).
-        cropped_sat = self.img_processor.crop_sat(sat_vgg_torch, correlation_orient, grd_vgg_torch.size()[-1])
-        sat_mat = nn.functional.normalize(cropped_sat, p=2, dim=(2, 3, 4))
-        distance = 2 - 2 * (torch.sum(sat_mat * grd_vgg_torch.unsqueeze(dim=0), dim=(2, 3, 4))).T
-
-        corr_out, corr_orien = corr(sat_vgg, grd_vgg)
-        sat_cropped = crop_sat(sat_vgg, corr_orien, grd_vgg.get_shape().as_list()[2])
-        # shape = [batch_sat, batch_grd, h, grd_width, channel]
-        sat_matrix = tf.nn.l2_normalize(sat_cropped, axis=[2, 3, 4])
-        distance = 2 - 2 * tf.transpose(
-            a=tf.reduce_sum(input_tensor=sat_matrix * tf.expand_dims(grd_vgg, axis=0), axis=[2, 3, 4]))
-        # shape = [batch_grd, batch_sat]
-
-        # If you print the distances mean, you'll see that they differ for a very small amount. Try.
-
-        # return sat_matrix, distance, corr_orien  # shapes: (1,1,4,64,16), (1,1), (1,1)
-        return sat_matrix, distance, corr_orien  # shapes: (10,10,4,64,16), (10,10), (10,10) 
-
     def forward(self, ground_view, satellite_view, satellite_segmented):
         if not self.input_is_transformed:
             satellite_view = torch.stack([self.img_processor.polar(img) for img in satellite_view.unbind(0)])
@@ -163,11 +72,5 @@ class SAN(nn.Module):
         cropped_sat = self.img_processor.crop_sat(concat, correlation_orient, vgg_ground_out.size()[-1])
         sat_mat = nn.functional.normalize(cropped_sat, p=2, dim=(2, 3, 4))
         distance = 2 - 2 * (torch.sum(sat_mat * vgg_ground_out.unsqueeze(dim=0), dim=(2, 3, 4))).T
-
-        # transform in tensorflow: in tf_correl the input tensors are tensorflow's tensor. Above the tensors are torch's tensors. With Maffo's code, the tensors must be in Pytorch's Framework. 
-        # Here, I first convert all in Tensorflow's tensor, permuting the shape. Then in tf_correl, when we convert all in Pytorch's tensors, we must permute the shape! -> Before: (batch, H, W, C), even if they were Pytorch tensors. Now: (batch, C, H, W)!
-        # vgg_ground_out_tf = tf.transpose(tf.convert_to_tensor(vgg_ground_out.detach().numpy()), perm=[0, 2, 3, 1])
-        # concat_tf = tf.transpose(tf.convert_to_tensor(concat.detach().numpy()), perm=[0, 2, 3, 1])
-        # sat, distance, orien = self.tf_correl(vgg_ground_out_tf, concat_tf)
 
         return distance
